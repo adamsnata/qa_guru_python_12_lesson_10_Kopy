2f0d7ec5c721347086e0cabd9220d53e8bf4b118
natal <svetnata74@mail.ru>
2024-04-19 14:41:32 +0200

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture()
+def browser_management():
+
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+    yield
+
+    browser.quit()
